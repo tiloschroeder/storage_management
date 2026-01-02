e7bdcd8879c3ab9cf313f6804b29ae17108ddae2
Tilo Schröder <me@tiloschroeder.de>
2026-01-02 15:21:48 +0100

    Change class name after renaming extension
    - Class name has been changed to match the name of the extension
    - The slug of the backend page has been changed to `view`

--- extension.driver.php
+++ extension.driver.php
@@ -1,94 +1,96 @@
 <?php
-	/*
-	Copyright: Deux Huit Huit 2013-2014
-	License: MIT, http://deuxhuithuit.mit-license.org
-	*/
+/**
+ * Copyright: Deux Huit Huit 2013-2014
+ * Copyright: Sym8 since 2025
+ * License: MIT, http://deuxhuithuit.mit-license.org
+ */
 
-	if(!defined("__IN_SYMPHONY__")) die("<h2>Error</h2><p>You cannot directly access this file</p>");
+if (!defined("__IN_SYMPHONY__")) {
+    die("<h2>Error</h2><p>You cannot directly access this file</p>");
+}
 
-	/**
-	 *
-	 * @author Deux Huit Huit
-	 * http://www.deuxhuithuit.com
-	 *
-	 */
-	class extension_cache_management extends Extension {
+class extension_storage_management extends Extension
+{
+    /**
+     * Name of the extension
+     * @var string
+     */
+    const EXT_NAME = 'Storage Management';
 
-		/**
-		 * Name of the extension
-		 * @var string
-		 */
-		const EXT_NAME = 'Cache Management';
+    /*********** DELEGATES ***********************/
 
-		/*********** DELEGATES ***********************/
-		
-		public function getSubscribedDelegates(){
-			return array(
-				array(
-					'page' => '/backend/',
-					'delegate' => 'NavigationPreRender',
-					'callback' => 'navigationPreRender'
-				)
-			);
-		}
-		
-		/**
-		 * Delegate fired to add a link to Cache Management
-		 */
-		public function fetchNavigation() {
-			if (is_callable(array('Symphony', 'Author'))) {
-					$author = Symphony::Author();
-			} else {
-					$author = Administration::instance()->Author;
-			}
-			
-			// Work around single group limit in nav
-			$group = $author->isDeveloper() ? 'developer' : 'manager';
-			
-			return array(
-					array (
-						'location' => __('System'),
-						'name' => __(self::EXT_NAME),
-						'link' => 'cache_management',
-						'limit' => $group,
-					) // nav group
-			); // nav
-		}
-		
-		public function navigationPreRender($context) {
-			$c = Administration::instance()->getPageCallback();
-			if ($c['driver'] == 'cache_management') {
-				foreach ($context['navigation'] as $key => $section) {
-					if ($section['name'] == 'System') {
-						$context['navigation'][$key]['class'] = 'active';
-					}
-				}
-			}
-		}
+    public function getSubscribedDelegates()
+    {
+        return array(
+            array(
+                'page' => '/backend/',
+                'delegate' => 'NavigationPreRender',
+                'callback' => 'navigationPreRender'
+            )
+        );
+    }
 
+    /**
+     * Delegate fired to add a link to Cache Management
+     */
+    public function fetchNavigation()
+    {
+        if (is_callable(array('Symphony', 'Author'))) {
+            $author = Symphony::Author();
+        } else {
+            $author = Administration::instance()->Author;
+        }
 
-		/* ********* INSTALL/UPDATE/UNISTALL ******* */
+        // Work around single group limit in nav
+        $group = $author->isDeveloper() ? 'developer' : 'manager';
 
-		/**
-		 * Creates the table needed for the settings of the field
-		 */
-		public function install() {
-			return true;
-		}
+        return array(
+                array (
+                    'location' => __('System'),
+                    'name' => __(self::EXT_NAME),
+                    'link' => 'view',
+                    'limit' => $group,
+                ) // nav group
+            ); // nav
+    }
 
-		/**
-		 * Creates the table needed for the settings of the field
-		 */
-		public function update($previousVersion = false) {
-			return true;
-		}
+    public function navigationPreRender($context)
+    {
+        $c = Administration::instance()->getPageCallback();
+        if ($c['driver'] == 'storage_management') {
+            foreach ($context['navigation'] as $key => $section) {
+                if ($section['name'] == 'System') {
+                    $context['navigation'][$key]['class'] = 'active';
+                }
+            }
+        }
+    }
 
-		/**
-		 *
-		 * Drops the table needed for the settings of the field
-		 */
-		public function uninstall() {
-			return true;
-		}
+    /* ********* INSTALL/UPDATE/UNISTALL ******* */
 
-	}
+    /**
+     * Creates the table needed for the settings of the field
+     */
+    public function install()
+    {
+        return true;
+    }
+
+    /**
+     * Creates the table needed for the settings of the field
+     */
+    public function update($previousVersion = false)
+    {
+        return true;
+    }
+
+    /**
+     *
+     * Drops the table needed for the settings of the field
+     */
+    public function uninstall()
+    {
+        return true;
+    }
+
+}
